38b0394d180c5498d59d49663db47ad56af643b5
Zebb Prime <zebb.prime@gmail.com>
2013-11-05 11:16:09 +1030

    Added license information to the source files.

--- src/gen_datastructures.m
+++ src/gen_datastructures.m
@@ -1,4 +1,14 @@
 % Generate the data structures for the soundfile package
+%
+% This function uses the current libsndfile header files to generate
+% a data structure used to interface with the libsndfile version that
+% sndfile_interface is compiled against.
+%
+% v0.1 2013-11-05
+%
+% Copyright (c) 2013 Zebb Prime
+% License information appended to source
+
 function sfds = gen_datastructures( sndlibpath )
   [sfds.filetypes,sfds.datatypes,sfds.endiantypes,sfds.mode] ...
       = gen_formats( sndlibpath );
@@ -73,3 +83,28 @@ function [filetypes,datatypes,endiantypes,mode] ...
     mode.( lower( temp{ii}{1} ) ) = hex2dec( temp{ii}{2} );
   end
 end
+
+% Copyright (c) 2013, Zebb Prime
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are 
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright 
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in 
+%       the documentation and/or other materials provided with the distribution
+%       
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
--- src/make.m
+++ src/make.m
@@ -1,3 +1,12 @@
+% Make (build) the binaries and data structures to interface with libsndfile
+%
+% This function uses mex to compile the soundfile interface (make sure that
+% you have a working mex setup before calling.
+%
+% v0.1 2013-11-05
+%
+% Copyright (c) 2013 Zebb Prime
+% License information appended to source
 function make
 
 %% Default paths for the sndfile library
@@ -26,3 +35,28 @@ save sfds sfds;
 [~,name] = fileparts(fname{1});
 copyfile([name,'.',mexext],'../@soundfile');
 copyfile('sfds.mat','../@soundfile');
+
+% Copyright (c) 2013, Zebb Prime
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are 
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright 
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in 
+%       the documentation and/or other materials provided with the distribution
+%       
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
