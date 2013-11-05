fc22c22e1e26dc2a04686581d4ee695de54451dd
Zebb Prime <zebb.prime@gmail.com>
2013-11-05 10:56:07 +1030

    Added license to the class_handle.hpp file.

--- src/class_handle.hpp
+++ src/class_handle.hpp
@@ -1,3 +1,12 @@
+/**
+ * Header file to enable Matlab to interface with c++ objects.
+ *
+ * This is taken from the Mathworks FileExchange submission:
+ * "Example MATLAB class wrapper for a C++ class"
+ *  http://mathworks.com.au/matlabcentral/fileexchange/38964
+ *
+ * See Copyright and author notice appended to this file.
+ */
 #ifndef __CLASS_HANDLE_HPP__
 #define __CLASS_HANDLE_HPP__
 #include "mex.h"
@@ -51,3 +60,30 @@ template<class base> inline void destroyObject(const mxArray *in)
 }
 
 #endif // __CLASS_HANDLE_HPP__
+
+/*
+Copyright (c) 2012, Oliver Woodford
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are 
+met:
+
+    * Redistributions of source code must retain the above copyright 
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright 
+      notice, this list of conditions and the following disclaimer in 
+      the documentation and/or other materials provided with the distribution
+      
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+*/
